1066f82087b55e95fcd9b9640e76f8a0f8d8acb4
chan9yu <dev.cgyeo@gmail.com>
2025-03-09 18:16:41 +0900

    chore: init project

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Test from "@/shared/components/Test";
+
+export default function RootPage() {
+	return (
+		<div className="h-dvh w-dvw">
+			<h1 className="flex h-full w-full items-center justify-center">Root Page</h1>
+			<Test />
+		</div>
+	);
+}
