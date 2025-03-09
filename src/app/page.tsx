2ac3bb07bdb0d6bd7c70adc58a9c04bbad403a30
chan9yu <dev.cgyeo@gmail.com>
2025-03-09 18:29:32 +0900

    feat: pretendard 폰트 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Test from "@/shared/components/Test";
 
 export default function RootPage() {
 	return (
-		<div className="h-dvh w-dvw">
-			<h1 className="flex h-full w-full items-center justify-center">Root Page</h1>
+		<div className="flex h-full w-full flex-col items-center justify-center gap-5">
+			<h1 className="text-xl">Root Page</h1>
 			<Test />
 		</div>
 	);
